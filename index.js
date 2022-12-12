950faec4821e0358f1bc1a47e2171d5c4b900c3b
edmilson <edneto.mn@gmail.com>
2022-12-12 10:38:34 -0300

    logica

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Hello world");
+console.log(15 + 33);
+
+
+
+let nome = "Edmilson Miranda";
+const dataNascimento = "30/07/1996";
+
+document.write('<h1 class= "bemm-vindo">Bem-vindo '+nome+'</h1>');
+
+
+document.write("<h1>Hello world</h1>");
+document.getElementsByTagName("h1")[0].style.color = "red";
+document.write(nome);
+document.write("<br>");
+nome = "Suelen";
+document.write(nome);
+document.write(dataNascimento);
